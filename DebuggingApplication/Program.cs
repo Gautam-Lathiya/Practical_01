fe8762a5af71333787ea72a9bbea5dc9d9b1a8ef
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-02-25 11:43:45 +0530

    Add DebuggingApplication codebase

---
+++ DebuggingApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter something: ");
+            string userResponse = Console.ReadLine();
+            try
+            {
+                if (Convert.ToBoolean(int.Parse(userResponse)))
+                {
+
+                    Console.WriteLine("The entered value is a integer");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The entered value is not a integer");
+            }
+        }
+    }
+}
