166a196bc3f0c1a4e94194bcf032a697310f7c6f
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-02-25 10:30:28 +0530

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = ReadNumber("Enter the first number : ");
+            int num2 = ReadNumber("Enter the second number : ");
+            Console.WriteLine($"The sum of {num1} and {num2} is {Calculator.Add(num1, num2)}");
+            Console.WriteLine($"The subtraction of {num1} and {num2} is {Calculator.Subtract(num1, num2)}");
+            Console.WriteLine($"The multiplication of {num1} and {num2} is {Calculator.Multiply(num1, num2)}");
+            try
+            {
+                Console.WriteLine($"The division of {num1} and {num2} is {Calculator.Divide(num1, num2)}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static int ReadNumber(string msg)
+        {
+            Console.WriteLine(msg);
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Invalid input. Please enter a valid integer : ");
+            }
+
+            return number;
+        }
+    }
+}
